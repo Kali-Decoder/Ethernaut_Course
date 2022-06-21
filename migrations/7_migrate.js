84d404615f4e0c1a69d60165c09578553d0bb1c4
Kali-Decoder <neerajchoubisa876@gmail.com>
2022-06-21 20:23:25 +0530

    Completing testing of election smart contract

---
+++ migrations/7_migrate.js
@@ -0,0 +1,5 @@
+const Factory= artifacts.require("ElectionFactory");
+
+module.exports=function(deployer){
+    deployer.deploy(Factory);
+}
--- test/whole.test.js
+++ test/whole.test.js
@@ -3,12 +3,15 @@ const Fallout = artifacts.require("Fallout");
 const CoinFlip = artifacts.require("CoinFlip");
 const Telephone = artifacts.require("Telephone");
 const SimpleToken= artifacts.require("SimpleToken");
+const ElectionFactory = artifacts.require("ElectionFactory.sol");
+const Election = artifacts.require("Election.sol");
 let simpleToken;
 let fallback;
 let fallout;
 let coinflip;
 let telephone;
-
+let electionFactory;
+let election;
 contract("Testing smart contract",(accounts)=>{
     beforeEach(async ()=>{
         fallback= await Fallback.deployed();
@@ -17,7 +20,13 @@ contract("Testing smart contract",(accounts)=>{
         coinflip= await CoinFlip.deployed();
         telephone= await Telephone.deployed({from:accounts[2]});
         simpleToken= await SimpleToken.deployed(18000,"Deccan Changer","DCT");
+        electionFactory= await ElectionFactory.deployed();
+        await electionFactory.createElection("neerajchoubisa876@gmail.com","Student of the year","For choosing student of year");
 
+        let electionData = await electionFactory.getDeployedElection("neerajchoubisa876@gmail.com");
+
+        election =await Election.at(electionData.deployedAddress);
+        
     });
     // it("Should test first smart contract ",async ()=>{
     //     let owner= await fallback.owner();
@@ -96,22 +105,54 @@ contract("Testing smart contract",(accounts)=>{
     //     console.log(owner);
     // });
 
-    it("Should text Simple token contract",async()=>{
+    // it("Should text Simple token contract",async()=>{
+    //     let obj= {
+    //         name: await simpleToken.name(),
+    //         symbol:await simpleToken.symbol(),
+    //         owner: await simpleToken.tokenOwner(),
+    //         totalSupply: await simpleToken.totalSupply(),
+    //     }
+    //     let ownerTokens= await simpleToken.tokenHolders(obj.owner);
+    //     await simpleToken.transferTokens(accounts[1],5000);
+    //     let acc1Bal = await simpleToken.balanceOf({from:accounts[1]})
+    //     assert.equal(acc1Bal.toNumber(),5000);
+
+    //     await simpleToken.transferTokensFrom(accounts[1],accounts[2],2500);
+    //     let acc2Bal= await simpleToken.balanceOf({from:accounts[2]});
+    //     assert.equal(acc2Bal.toNumber(),2500);
+    // })
+    it("Should Testing Election Contract",async ()=>{
         let obj= {
-            name: await simpleToken.name(),
-            symbol:await simpleToken.symbol(),
-            owner: await simpleToken.tokenOwner(),
-            totalSupply: await simpleToken.totalSupply(),
+            electionName:await election.electionName(),
+            electionDesc:await election.electionDesc(),
+            electionAuthority:await election.electionAuthority(),
+            status:await election.status(),
         }
-        let ownerTokens= await simpleToken.tokenHolders(obj.owner);
-        await simpleToken.transferTokens(accounts[1],5000);
-        let acc1Bal = await simpleToken.balanceOf({from:accounts[1]})
-        assert.equal(acc1Bal.toNumber(),5000);
+        // adding Candidate ..... 
+        await election.addCandidate("Neeraj Choubisa","NC","www.nikku","neerajchoubisa876@gmail.com");
+        await election.addCandidate("Sneha Gupta","SG","www.sneha","snehA345@gmail.com");
+        await election.addCandidate("Tanmay Bhatt","TB","www.tanmay","gtanmay654@gmail.com");
+        let numCandidates= await election.getNumberCandidates();
+        await election.vote(1,"nee@gmail.com",{from:accounts[1]});
+        await election.vote(2,"mee@gmail.com",{from:accounts[2]});
+        await election.vote(3,"pee@gmail.com",{from:accounts[3]});
+        await election.vote(1,"qee@gmail.com",{from:accounts[4]});
+        await election.vote(2,"ree@gmail.com",{from:accounts[5]});
+        await election.vote(3,"see@gmail.com",{from:accounts[6]});
+        await election.vote(2,"tee@gmail.com",{from:accounts[7]});
+        await election.vote(2,"uee@gmail.com",{from:accounts[8]});
+        await election.vote(1,"vee@gmail.com",{from:accounts[9]});
+        let numVoters= await election.getNumberVoters();
+        assert.equal(9,numVoters.toNumber());
+        assert.equal(3,numCandidates.toNumber());
+        let candidateDetail= await election.getCandidateDetail(1);
+        let electionDetail = await election.getElectionDetail();
+        assert.equal(candidateDetail.id,1);
+        assert.equal(electionDetail[3],true);
+        let winner= await election.getWinner({from:accounts[0]});
+        let status= await election.status();
+        assert.equal(status,false);
 
-        await simpleToken.transferTokensFrom(accounts[1],accounts[2],2500);
-        let acc2Bal= await simpleToken.balanceOf({from:accounts[2]});
-        assert.equal(acc2Bal.toNumber(),2500);
     })
-
 
 })
